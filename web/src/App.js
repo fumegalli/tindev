e51b44e3dde8b4a3b337ec22f4e3ece543e48dcf
Fumegalli <rafaelxxc@gmail.com>
2019-08-07 14:17:38 -0300

    Calling routes in app.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './App.css'
 
-class App extends Component {
-    render() {
-        return <h1>Alo</h1>
-    }
+import Routes from './routes'
+
+function App() {
+    return <Routes />
 }
 
 export default App
