7603c0f34b48bd0e75dddddb8820387d804380b2
Rafael Fumegalli <rafael.fumegalli@cwi.com.br>
2019-08-07 20:37:44 -0300

    Add required to login input

--- web/src/pages/login/Login.js
+++ web/src/pages/login/Login.js
@@ -24,6 +24,7 @@ export default function Login({ history }) {
                 <input
                     placeholder='Digite seu usuário no Github'
                     value={username}
+                    required
                     onChange={e => setUsername(e.target.value)}
                 />
                 <button type='submit'> Entrar </button>
