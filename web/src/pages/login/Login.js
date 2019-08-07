fb60d18980fa96bdfbda3600e18cbc5dc450767d
Fumegalli <rafaelxxc@gmail.com>
2019-08-07 15:09:27 -0300

    Changing button text

--- web/src/pages/login/Login.js
+++ web/src/pages/login/Login.js
@@ -28,7 +28,7 @@ export default function Login({ history }) {
                     value={username}
                     onChange={e => setUsername(e.target.value)}
                 />
-                <button type="submit"> Enviar </button>
+                <button type="submit"> Entrar </button>
             </form>
         </div>
     )
