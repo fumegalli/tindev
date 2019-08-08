af3c9e59c384d35b3b80741474f2eabb5b48d7f2
Fumegalli <rafaelxxc@gmail.com>
2019-08-08 19:46:45 -0300

    Add routes.js

---
+++ app/src/routes.js
@@ -0,0 +1,9 @@
+import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+import { Login, Main } from './pages'
+
+export default createAppContainer(
+    createSwitchNavigator({
+        Login,
+        Main,
+    })
+)
