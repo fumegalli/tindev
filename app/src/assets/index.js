ea5b6118e57eb6d62e7b84573ee93ff2b9f4d55c
Fumegalli <rafaelxxc@gmail.com>
2019-08-09 16:57:25 -0300

    Add new asset

--- app/src/assets/index.js
+++ app/src/assets/index.js
@@ -1,5 +1,6 @@
 import logo from './logo.png'
 import like from './like.png'
 import dislike from './dislike.png'
+import itsAMatch from './itsamatch.png'
 
-export { logo, like, dislike }
+export { logo, like, dislike, itsAMatch }
