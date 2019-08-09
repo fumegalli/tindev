92a1dabb1f01663a21cf35e0faf31bf00a91b47d
Fumegalli <rafaelxxc@gmail.com>
2019-08-09 11:06:58 -0300

    Exporting itsAMatch

--- web/src/assets/index.js
+++ web/src/assets/index.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg'
 import dislike from './dislike.svg'
 import like from './like.svg'
+import itsAMatch from './itsamatch.png'
 
-export { logo, dislike, like }
+export { logo, dislike, like, itsAMatch }
